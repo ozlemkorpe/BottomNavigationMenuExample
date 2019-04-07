4ff36c9eae637b1bd2eb8d661549b82be49fb0e2
Ozlem <cse.ozlemkorpe@gmail.com>
2019-04-07 23:46:09 +0300

    Initial commit

---
+++ app/src/main/java/gelecegiyazanlar/com/gyk401/fragments/HealthFragment.java
@@ -0,0 +1,72 @@
+package gelecegiyazanlar.com.gyk401.fragments;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import gelecegiyazanlar.com.gyk401.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link HealthFragment.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link HealthFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class HealthFragment extends Fragment {
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    private OnFragmentInteractionListener mListener;
+
+    public HealthFragment() {
+        // Required empty public constructor
+    }
+
+
+    public static HealthFragment newInstance(String param1, String param2) {
+        HealthFragment fragment = new HealthFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view1 = inflater.inflate(R.layout.fragment_profile,null);
+        return view1;
+    }
+
+    public void onButtonPressed(Uri uri) {
+        if (mListener != null) {
+            mListener.onFragmentInteraction(uri);
+        }
+    }
+
+    public interface OnFragmentInteractionListener {
+        void onFragmentInteraction(Uri uri);
+    }
+}
